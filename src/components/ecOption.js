db984d6c03c325429c0e18193d2828aea278137a
Frank Wei <frank@frankwei.xyz>
2018-05-18 14:16:30 +0800

    rm: remove useless code

--- src/components/ecOption.js
+++ src/components/ecOption.js
@@ -18,8 +18,8 @@ export default function getEcOption(data) {
   ]
   const option = {
     xAxis: {
-      type: 'category',
-      data: ['10:00', '10:30', '11:00', '11:30', '13:00', '13:30', '14:00']
+      type: 'category'
+      // data: ['10:00', '10:30', '11:00', '11:30', '13:00', '13:30', '14:00']
     },
     yAxis: {
       type: 'value'
